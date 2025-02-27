4c797a489767b51223778a5e329074fa9ce40929
liwei <13641012119@163.com>
2025-02-27 18:58:15 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "", // 去掉基础路径
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://read-ssr.oss-cn-hangzhou.aliyuncs.com"
-      : "",
+  // assetPrefix:
+  //   process.env.NODE_ENV === "production"
+  //     ? "https://read-ssr.oss-cn-hangzhou.aliyuncs.com"
+  //     : "",
   env: {
     NEXT_PUBLIC_OPENAI_KEY: "sk-b741625a0a084b3195803bff0cb66327",
   },
